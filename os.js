291a63cb8bb8ed49c3ab30a80512d13dedce5968
zainab <zainab.malhi@outlook.com>
2024-08-24 10:05:37 -0400

    os module

---
+++ os.js
@@ -0,0 +1,10 @@
+const os = require("os");
+// You can import at the bottom also
+// but it will not look good
+// import at the top
+
+console.log(os.platform());
+
+console.log(os.hostname());
+console.log(os.freemem());
+console.log(os.homedir());
