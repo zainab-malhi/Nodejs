72d1653ddea6f033c98813f6263997f8a14472c0
zainab <zainab.malhi@outlook.com>
2024-08-27 12:50:00 -0400

    connecting to database

--- api-fundamentals/index.js
+++ api-fundamentals/index.js
@@ -11,7 +11,7 @@ http
     let parsedUrl = url.parse(req.url, true);
 
     let products = fs.readFileSync("./products.json", "utf-8");
-
+    //res.setHeader("Acess-Control-Allow-Origin","*")
     // fetching all products data
     if (
       parsedUrl.pathname == "/products" &&
@@ -41,9 +41,12 @@ http
     // create new product
     else if (parsedUrl.pathname == "/products" && req.method == "POST") {
       let product = "";
+      // this event is called at every chunk
       req.on("data", (chunk) => {
         product = product + chunk;
       });
+
+      // this event is called at the ned of streams and convert bytes to readeable string
       req.on("end", () => {
         let productArray = JSON.parse(products);
         let newProduct = JSON.parse(product);
@@ -58,6 +61,65 @@ http
             res.end(JSON.stringify({ message: "Some Problem" }));
           }
         });
+      });
+    }
+
+    // delete based on id
+    else if (parsedUrl.pathname == "/products" && req.method == "DELETE") {
+      let productArray = JSON.parse(products);
+
+      let index = productArray.findIndex((product) => {
+        return product.id == parsedUrl.query.id;
+      });
+
+      productArray.splice(index, 1);
+
+      fs.writeFile("./products.json", JSON.stringify(productArray), (err) => {
+        if (err == null) {
+          res.end(JSON.stringify({ message: "Product deleted suceessfully" }));
+        } else {
+          res.end(JSON.stringify({ message: "Some Problem" }));
+        }
+      });
+    }
+
+    //endpoint to update
+    else if (parsedUrl.pathname == "/products" && req.method == "PUT") {
+      let id = parsedUrl.query.id;
+      let product = "";
+
+      // this event is called at every chunk
+      req.on("data", (chunk) => {
+        product = product + chunk;
+      });
+
+      // this event is called at the end of streams and convert bytes to readeable string
+      req.on("end", () => {
+        let productArray = JSON.parse(products);
+        let productOBJ = JSON.parse(product);
+
+        let index = productArray.findIndex((product) => {
+          return product.id == id;
+        });
+
+        if (index != -1) {
+          productArray[index] = productOBJ;
+          fs.writeFile(
+            "./products.json",
+            JSON.stringify(productArray),
+            (err) => {
+              if (err == null) {
+                res.end(
+                  JSON.stringify({ message: "Element sucessfully updated" })
+                );
+              } else {
+                res.end(JSON.stringify({ message: "Some problem" }));
+              }
+            }
+          );
+        } else {
+          res.end(JSON.stringify({ message: "Element not found" }));
+        }
       });
     }
   })
